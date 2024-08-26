c0d7f006a33ebdbd3a3b8bca88e8eb3a153f9a32
Ecesu Demirkaya <109510876+ecesudemirkaya@users.noreply.github.com>
2024-08-26 14:22:05 +0300

    done current location layout setup

--- app/src/main/java/com/example/vb_weatherapp/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/vb_weatherapp/fragments/home/HomeFragment.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.vb_weatherapp.data.CurrentLocation
 import com.example.vb_weatherapp.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class HomeFragment: Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = requireNotNull(_binding)
+
+    private val weatherDataAdapter = WeatherDataAdapter(
+        onLocationClicked = {
+            Toast.makeText(requireContext(), "onLocationClicked()", Toast.LENGTH_SHORT).show()
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +30,25 @@ class HomeFragment: Fragment() {
         ): View {
         _binding = FragmentHomeBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setWeatherDataAdapter()
+        setWeatherData()
+    }
+
+    private fun setWeatherDataAdapter() {
+        binding.weatherDataRecyvlerView.adapter = weatherDataAdapter
+    }
+
+    private fun setWeatherData() {
+        weatherDataAdapter.setData(data = listOf(CurrentLocation(date = getCurrentDate())))
+    }
+
+    private fun getCurrentDate(): String {
+        val currentDate = Date()
+        val formatter = SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
+        return "Today, ${formatter.format(currentDate)}"
     }
 }
